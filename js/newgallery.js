78a3df7a95f5fa3731b2ee59c4dcf0931e95f331
Santiago Miqueo <smiqueo@hotmail.com>
2022-08-18 15:01:03 -0300

    Change gallery again

---
+++ js/newgallery.js
@@ -0,0 +1,16 @@
+let thumbnails = document.getElementsByClassName("thumbnail");
+
+let activeImages = document.getElementsByClassName("active");
+
+for (var i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener("mouseover", function () {
+    console.log(activeImages);
+
+    if (activeImages.length > 0) {
+      activeImages[0].classList.remove("active");
+    }
+
+    this.classList.add("active");
+    document.getElementById("featured").src = this.src;
+  });
+}
